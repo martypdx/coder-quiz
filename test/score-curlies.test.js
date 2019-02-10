de6873ead08d75e46b6fbaaccbd611ff867b9c11
martypdx <marty@martypdx.com>
2019-02-10 09:53:08 -0800

    use shorter test names as we now have module names

--- test/score-curlies.test.js
+++ test/score-curlies.test.js
@@ -8,17 +8,17 @@ QUnit.testStart(function() {
     scorecard = { js: 0, python: 0, java: 0, csharp: 0 };
 });
 
-test('curlies love', function(assert) {
+test('love', function(assert) {
     scoreCurlies('love', scorecard);
     assert.deepEqual(scorecard, { js: 1, python: 0, java: 0.5, csharp: 0.5 });
 });
 
-test('curlies okay', function(assert) {
+test('okay', function(assert) {
     scoreCurlies('okay', scorecard);
     assert.deepEqual(scorecard, { js: 0.5, python: 0, java: 1, csharp: 1 });
 });
 
-test('curlies indent', function(assert) {
+test('indent', function(assert) {
     scoreCurlies('indent', scorecard);
     assert.deepEqual(scorecard, { js: 0, python: 1, java: 0, csharp: 0 });
 });
--- test/score-verbosity.test.js
+++ test/score-verbosity.test.js
@@ -8,17 +8,17 @@ QUnit.testStart(function() {
     scorecard = { js: 0, python: 0, java: 0, csharp: 0 };
 });
 
-test('verbosity brevity', function(assert) {
+test('brevity', function(assert) {
     scoreVerbosity('brevity', scorecard);
     assert.deepEqual(scorecard, { js: 1, python: 0.5, java: 0, csharp: 0 });
 });
 
-test('verbosity dynamic', function(assert) {
+test('dynamic', function(assert) {
     scoreVerbosity('dynamic', scorecard);
     assert.deepEqual(scorecard, { js: 1, python: 1, java: 0, csharp: 0 });
 });
 
-test('verbosity dynamic', function(assert) {
+test('dynamic', function(assert) {
     scoreVerbosity('verbose', scorecard);
     assert.deepEqual(scorecard, { js: 0, python: 0, java: 1, csharp: 1 });
 });
