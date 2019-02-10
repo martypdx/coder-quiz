dcfed9b4f5e1a5f0963e103ff4bcb1272ccc79b4
martypdx <marty@martypdx.com>
2019-02-10 09:35:12 -0800

    refactor to use common test setup for scorecard

--- test/tests.js
+++ test/tests.js
@@ -11,14 +11,17 @@ function scoreVerbosity(answer, scorecard) {
     }
 }
 
+let scorecard = null;
+QUnit.testStart(function() {
+    scorecard = { js: 0, python: 0, java: 0, csharp: 0 };
+});
+
 test('verbosity brevity', function(assert) {
-    const scorecard = { js: 0, python: 0, java: 0, csharp: 0 };
     scoreVerbosity('brevity', scorecard);
     assert.deepEqual(scorecard, { js: 1, python: 0.5, java: 0, csharp: 0 });
 });
 
 test('verbosity dynamic', function(assert) {
-    const scorecard = { js: 0, python: 0, java: 0, csharp: 0 };
     scoreVerbosity('dynamic', scorecard);
     assert.deepEqual(scorecard, { js: 1, python: 1, java: 0, csharp: 0 });
 });
