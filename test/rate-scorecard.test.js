648e479da8e0127809e2e577a7c120ae3e1c6414
martypdx <marty@martypdx.com>
2019-02-10 12:50:53 -0800

    first three passing tests for rate scorecard

--- test/rate-scorecard.test.js
+++ test/rate-scorecard.test.js
@@ -3,13 +3,42 @@ const test = QUnit.test;
 QUnit.module('rate scorecard');
 
 function rateScorecard(scorecard) {
+    const js = scorecard.js;
+    const python = scorecard.python;
+    const java = scorecard.java;
+    const csharp = scorecard.csharp;
 
+    if(js > python && js > java && js > csharp) {
+        return 'JavaScript';
+    }
+
+    if(python > js && python > java && python > csharp) {
+        return 'Python';
+    }
+
+    if(java > js && java > python && java > csharp) {
+        return 'Java';
+    }
 }
 
 test('JavaScript', function(assert) {
     const result = rateScorecard({ 
-        js: 3, python: 2, java: 1, csharp: 0
+        js: 1, python: 0, java: 0, csharp: 0
     });
     assert.equal(result, 'JavaScript');
+});
+
+test('Python', function(assert) {
+    const result = rateScorecard({ 
+        js: 0, python: 1, java: 0, csharp: 0
+    });
+    assert.equal(result, 'Python');
+});
+
+test('Java', function(assert) {
+    const result = rateScorecard({ 
+        js: 0, python: 0, java: 1, csharp: 0
+    });
+    assert.equal(result, 'Java');
 });
 
